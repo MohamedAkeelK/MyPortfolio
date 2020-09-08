a785b11eda7a5e0021a016d1b00007f370fd62d8
unknown <Mohamedakeelkhan@gmail.com>
2020-09-08 05:58:36 -0400

    updated images, added profile new portrait photo

--- client/src/banner.js
+++ client/src/banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import avatar from "./avatar.jpg";
+import avatar from "./portait.jpg";
 // import bannerImg from "./ny.jpg";
 
 const Banner = (props) => {
@@ -8,15 +8,18 @@ const Banner = (props) => {
       <div className="banner">
         <div className="contain">
           <img src={avatar} />
-          <h1>Welcome!</h1>
-          <p>
+          <h1>WELCOME!</h1>
+          {/* <p>
             Hi, My name is Mohamed Akeel Khan and i am a Computer Science
             student at Queensborough Community College with a passion for tech
             and a desire to learn. Currently in a joint program with John Jay
             College of Criminal Justice - Majoring in Computer Science and
             Information Security. Seeking to gain a position with a growing and
             innovative company.
-          </p>
+          </p> */}
+          <a className="cta" href="/">
+            <button>Contact</button>
+          </a>
         </div>
         {/* <img className="banner" src={bannerImg}></img> */}
       </div>
